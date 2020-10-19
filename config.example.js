f25fcfa301437832ad9e2aa2d8a4b81d76cda615
FC5570 <68158483+FC5570@users.noreply.github.com>
2020-10-19 16:28:13 +0530

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,11 @@
+exports.token = "Your bot token";
+exports.prefix = ";";
+exports.spCID =  "Client Id";
+exports.spCS =  "Client Secret";
+exports.nodes = [
+    {
+        port: "2333",
+        host: "localhost",
+        password: "youshallnotpass"
+        },
+    ]
